ae24bb928f1265d6e7a07e0230d99c14035dea5d
Christian Martins <christian@labdynamo.com>
2020-03-18 13:50:56 -0400

    quick commit

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,16 +1,39 @@
 import React from "react";
 import { StyleSheet, Text, FlatList } from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { ProductItem } from "../../components/shop";
+import * as cartActions from "../../store/actions/cart";
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
+  const dispatch = useDispatch();
+
   return (
     <FlatList
       data={products}
       keyExtractor={item => item.id}
-      renderItem={itemData => <Text>{itemData.item.title}</Text>}
+      renderItem={itemData => (
+        <ProductItem
+          title={itemData.item.title}
+          image={itemData.item.imageUrl}
+          price={itemData.item.price}
+          onViewDetail={() => {
+            props.navigation.navigate("ProductDetail", {
+              productId: itemData.item.id,
+              productTitle: itemData.item.title
+            });
+          }}
+          onAddToCart={() => {
+            dispatch(cartActions.addToCart(itemData.item));
+          }}
+        />
+      )}
     />
   );
+};
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: "All Products"
 };
 
 const styles = StyleSheet.create({});
